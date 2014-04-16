df323ddd8324e46ae1ce8a0bfe8004481049b12a
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2014-04-16 12:46:49 +0300

    Add specsuite as well

---
+++ SpecsSuite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpecsSuite
+//
+//  Created by lauris on 4/16/14.
+//  Copyright (c) 2014 greenhouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
